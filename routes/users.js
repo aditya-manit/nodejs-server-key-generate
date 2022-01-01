6c758737e9357d93779973f8fc6c2adb2d78cb2e
Aditya Verma <aditya.verma@flipkart.com>
2022-01-01 08:53:42 +0530

    change to json

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,16 @@ var shell = require('shelljs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // let output = shell.exec(`cat output.txt`, {async: false}).stdout;
-  let output = shell.exec(`evmosd keys add test --keyring-backend test`, {async: false}).stdout;
-  let remove = shell.exec(`rm -r .evmosd/keyring-test/`, {async: false}).stdout;
-  output = output.replace(/\n/g, " ");
-  let arr = output.split(" ");
-  let mnemonic = [];
-  for (i = arr.length -1; i > arr.length -25 ;i--) {
-    mnemonic.push(arr[i])
-  }
-  mnemonic.reverse()
-  console.log(mnemonic)
+  let output = shell.exec(`evmosd keys add test --keyring-backend test --log_format json --output json`, {async: false}).stdout;
+  output = JSON.parse(output)
+  console.log(output)
+  let remove = shell.exec(`rm -r ~/.evmosd/keyring-test/`, {async: false}).stdout;
+  let mnemonicArr = output.mnemonic.split(" ");
+  console.log(mnemonicArr)
   res.send(
       {
-        "address": arr[11],
-        "mnemonic": mnemonic
+        "address": output.address,
+        "mnemonic": mnemonicArr
       }
   );
 });
